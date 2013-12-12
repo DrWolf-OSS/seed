180c7ee706ab96600414fe06f41cc7d14b59cb6e
Lorenzo Riganella <lorenzo.riganella@drwolf.com>
2013-12-12 16:14:45 +0100

    routes and controllers for landslip

---
+++ app/controllers/Landslip.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+public class Landslip extends Controller {
+
+  public static Result landslips() {
+    return TODO;
+  }
+
+  public static Result newLandslip(){
+    return TODO;
+  }
+
+  public static Result deleteLandslip(Long id){
+    return TODO;
+  }
+}
