527dc2756c02d5f4301f42426a573b7678db6ba1
Lorenzo Riganella <lorenzo.riganella@drwolf.com>
2013-12-12 16:14:45 +0100

    routes and controllers for landslip

---
+++ app/models/Landslip.java
@@ -0,0 +1,25 @@
+package models;
+
+import java.utils.*;
+
+public class Landslip {
+
+  public Long id;
+  public String name;
+
+  /*@Column(columnDefinition = "TEXT")*/
+  private String description;
+
+  public static List<Landslip> all(){
+    return new arraylist<Landslip>();
+  }
+
+  public static void create(Landslip landslip){
+
+  }
+
+  public static void delete(Long id){
+
+  }
+
+}
