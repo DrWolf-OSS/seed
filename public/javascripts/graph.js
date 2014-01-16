554d0f304e4d6f9b14b9d84ee4c652e5bc557fa2
Lorenzo Riganella <lorenzo.riganella@drwolf.com>
2014-01-16 17:46:48 +0100

    adde lights and ground

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -27,8 +27,51 @@ function init(positions){
   // world
   scene = new THREE.Scene();
   scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+ 
+  // LIGHTS
   
+  hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+  hemiLight.color.setHSL( 0.6, 1, 0.6 );
+  hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+  hemiLight.position.set( 0, 500, 0 );
+  scene.add( hemiLight );
+
   
+
+  dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  dirLight.color.setHSL( 0.1, 1, 0.95 );
+  dirLight.position.set( -1, 1.75, 1 );
+  dirLight.position.multiplyScalar( 50 );
+  scene.add( dirLight );
+
+  dirLight.castShadow = true;
+
+  dirLight.shadowMapWidth = 2048;
+  dirLight.shadowMapHeight = 2048;
+  var d = 50;
+
+  dirLight.shadowCameraLeft = -d;
+  dirLight.shadowCameraRight = d;
+  dirLight.shadowCameraTop = d;
+  dirLight.shadowCameraBottom = -d;
+
+  dirLight.shadowCameraFar = 3500;
+  dirLight.shadowBias = -0.0001;
+  dirLight.shadowDarkness = 0.35;
+  dirLight.shadowCameraVisible = true;
+
+  // GROUND
+
+  var groundGeo = new THREE.PlaneGeometry( 10000, 10000 );
+  var groundMat = new THREE.MeshPhongMaterial( { ambient: 0xffffff, color: 0xffffff, specular: 0x050505 } );
+  groundMat.color.setHSL( 0.095, 1, 0.75 );
+
+  var ground = new THREE.Mesh( groundGeo, groundMat );
+  ground.rotation.x = -Math.PI/2;
+  ground.position.y = -33;
+  scene.add( ground );
+
+  ground.receiveShadow = true; 
   makeParticles(positions);
   
 
