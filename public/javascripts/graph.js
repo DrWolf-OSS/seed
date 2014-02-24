81c6e871d86db04ac56c9235566e6ee34728ba56
Lorenzo Riganella <lorenzo.riganella@drwolf.com>
2014-02-24 12:33:36 +0100

    lib in local, trackball controls only in canvas area

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -6,6 +6,11 @@
   var tweenSpeed, tweenPlay;
   var transparencySpeed;
 
+  var mouse = new THREE.Vector2(),
+  offset = new THREE.Vector3(),
+  INTERSECTED, SELECTED;
+
+
   function Marker(particle, tweens, currentPosition){
     this.particle = particle;
     this.tweens = tweens;
@@ -43,8 +48,6 @@ function init(p){
   camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
   camera.position.z = 500;
 
-  //CONTROLS
-  makeControls();
  
   scene.add( new THREE.AmbientLight( 0x202020 ) );
   
@@ -76,6 +79,8 @@ function init(p){
 
   // renderer
   initRenderer();
+  //CONTROLS
+  makeControls();
   animate();
 };
 
@@ -89,7 +94,7 @@ function render() {
 /* Controls setup */
 function makeControls(){
 
-  controls = new THREE.TrackballControls( camera );
+  controls = new THREE.TrackballControls( camera, renderer.domElement);
   controls.rotateSpeed = 1.0;
   controls.zoomSpeed = 1.2;
   controls.panSpeed = 0.8;
@@ -97,8 +102,8 @@ function makeControls(){
   controls.noPan = false;
   controls.staticMoving = true;
   controls.dynamicDampingFactor = 0.3;
-  controls.keys = [ 65, 83, 68 ];
-  controls.addEventListener( 'change', render );
+  //controls.keys = [ 65, 83, 68 ];
+  //controls.addEventListener( 'change', render );
 }
 
 /*Renders ground */
@@ -385,4 +390,27 @@ function moveSlider(value){
   
   $slider = $('#slider');
   $slider.slider('setValue', value);
+}
+
+
+function onDocumentMouseDown( event ) {
+
+  event.preventDefault();
+
+  var vector = new THREE.Vector3( mouse.x, mouse.y, 0.5 );
+  projector.unprojectVector( vector, camera );
+
+  var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+  var intersects = raycaster.intersectObjects( objects );
+
+  if ( intersects.length > 0 ) {
+
+    controls.enabled = false;
+
+    SELECTED = intersects[ 0 ].object;
+
+
+  }
+
 }
